82d8260a0fcad30832d0f10d90e42099b1f46e00
tyasheliy <bardaksss3@gmail.com>
2023-02-11 23:41:12 +0300

    pudgerios.client added

---
+++ pudgerios.client/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<Navbar />
+	<div class = 'container'>
+		
+	</div>
+</template>
+
+<script>
+	import Navbar from './components/Navbar.vue'
+
+	export default{
+		components: {
+			Navbar
+		}
+	}
+</script>
+
+<style>
+	@import './static/css/bootstrap.min.css';
+
+	body{
+		font-family: 'Alice';
+		font-weight: 400;
+		font-style: normal;
+	}
+</style>
---
+++ pudgerios.client/src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+	<nav :class="(transparent) ? 'navbar navbar-expand-lg navbar-dark bg-transparent' : 'navbar navbar-expand-lg navbar-dark bg-dark'">
+		<div class = 'container'>
+			<a href = '#' class = 'navbar-brand'>Pudgerios Hotel</a>
+			<ul class='navbar-nav me-auto mx-3 mb-2 mb-lg-0'>
+				<li class='nav-item'><a href = '#' class = 'nav-link'>Об отеле</a></li>
+				<li class = 'nav-item'><a href = '#' class = 'nav-link'>События</a></li>
+				<li class = 'nav-item'><a href = '#' class = 'nav-link'>Номера</a></li>
+				<li class = 'nav-item'><a href = '#' class = 'nav-link'>Питание и развлечение</a></li>
+				<li class = 'nav-item'><a href = '#' class = 'nav-link'>Контакты</a></li>
+			</ul>
+			<hr v-if="transparent" color = "white" size = "2px" width = "100%">
+		</div>
+	</nav>
+</template>
+
+<script>
+export default{
+	props: {
+		transparent: Boolean
+	}
+}
+</script>
+<style>
+	.navbar-brand.navbar-brand {
+		font-size: 36px;
+	}
+
+	.nav-link.nav-link {
+		color: white;
+		font-size: 26px;
+	}
+
+	.nav-item.nav-item {
+		margin-left: 6px;
+	}
+
+	.dark {
+		position: sticky;
+		top: 0;
+		left: 0;
+	}
+
+	.transparent {
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+</style>
