eb3a5ea14072d2cdb1bc4bdc2f11bebc275a4853
tyasheliy <bardaksss3@.gmail.com>
2023-02-15 00:38:48 +0300

    frame 3 finished

---
+++ pudgerios.client/src/components/Typecard.vue
@@ -0,0 +1,113 @@
+<template>
+	<div class='type-card text-dark'>
+		<img :src='validate_image_src()' />
+		<div class='type-card-body'>
+			<p class='type-card-caption'>{{caption}}</p>
+			<span class='type-card-view'>
+				<img src='../static/img/view.jpg' />
+				{{view}}
+			</span>
+			<span class='type-card-size'>
+				<img src='../static/img/size.jpg' />
+				{{size}}
+			</span>
+			<span class='type-card-suits'>
+				<img src='../static/img/suits.jpg' />
+				{{suits}}
+			</span>
+			<span class='type-card-food'>
+				<img src='../static/img/food.jpg' />
+				{{food}}
+			</span>
+		</div>
+		<div class='type-card-footer'>
+			<a :href='link' class='btn btn-light shadow' role='button'>{{validate_button_text()}}</a>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			image: String,
+			caption: String,
+			view: String,
+			size: String,
+			suits: String,
+			food: String,
+			link: String,
+			button_text: String,
+		},
+		methods: {
+			validate_button_text(){
+				if (this.button_text === undefined){
+					return "Подробнее";
+				}
+				return this.button_text;
+			},
+			validate_image_src(){
+				try {
+					return require("../static/img/" + this.image);
+				}
+				catch {
+					return require("../static/img/luxe.png");
+				}
+			},
+		}
+	}
+</script>
+
+<style scoped>
+	.type-card {
+		position: relative;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: white;
+	}
+
+	.type-card img {
+		display: block;
+		position: relative;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 50%;
+		filter: none;
+		z-index: 1;
+	}
+
+	.type-card-caption {
+		font-weight: bold;
+		font-size: 150%;
+	}
+
+	.type-card-body {
+		position: relative;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 40%;
+	}
+
+	.type-card-body span {
+		display: block;
+		margin: 0 1% 0 1%;
+	}
+	
+	.type-card-body img {
+		display: inline;
+		width: 27px;
+		height: 27px;
+		z-index: 1;
+	}
+
+	.type-card-footer {
+		position: relative;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 10%;
+	}
+</style>
--- pudgerios.client/src/views/Index.vue
+++ pudgerios.client/src/views/Index.vue
@@ -1,42 +1,109 @@
 <template>
-	<div class = 'frame frame-1 text-center text-light'>
+	<div class='frame frame-1 text-center text-light'>
 		<Navbar transparent />
-		<span class = 'display-1 caption main-caption border border-light'>PUDGERIOS Hotel</span><br>
-		<span class = 'display-2 caption'>Восторг без границ<br>Комфорт без компромиссов</span>		
+		<span class='display-1 caption main-caption border border-light'>PUDGERIOS Hotel</span><br>
+		<span class='display-2 caption'>Восторг без границ<br>Комфорт без компромиссов</span>
 
-		<img src = '../static/img/index1.png'>
+		<img src='../static/img/index1.png'>
 	</div>
-	<div class = 'frame frame-2 text-light'>
-		<span class = 'caption border border-light display-4 my-4 mx-4'>PUDGERIOS Hotel</span>
-		<p class = 'description-1'>Уютный и небольшой, окутанный атмосферой непринужденности, в которой каждый гость почувствует себя комфортно, отель располагает общими помещениями — холлом, администрацией и баром — отреставрированными умелой рукой венецианского архитектора Карло Скарпа.</p>
-		<p class = 'description-2'>Кроме удобного лифта на верхние этажи ведет великолепная лестница — тоже произведение Скарпа. Тут вас ждут со вкусом оформленные номера с лучшими удобствами, которые обеспечат приятный отпуск.</p>
-		<img src = '../static/img/index2.png'>
+	<div class='frame frame-2 text-light'>
+		<span class='caption border border-light display-4 my-4 mx-4'>PUDGERIOS Hotel</span>
+		<p class='description-1'>Уютный и небольшой, окутанный атмосферой непринужденности, в которой каждый гость
+			почувствует себя комфортно, отель располагает общими помещениями — холлом, администрацией и баром —
+			отреставрированными умелой рукой венецианского архитектора Карло Скарпа.</p>
+		<p class='description-2'>Кроме удобного лифта на верхние этажи ведет великолепная лестница — тоже произведение
+			Скарпа. Тут вас ждут со вкусом оформленные номера с лучшими удобствами, которые обеспечат приятный отпуск.
+		</p>
+		<img src='../static/img/index2.png'>
 	</div>
-	<div class = 'd-flex justify-content-center'>
-		<div class = 'index-form'>
-			aboba
+	<div class='index-form'>
+		<form>
+			<div class='row'>
+				<div class='col-3'>
+					<span>Забронировать</span>
+				</div>
+				<div class='col-3'>
+					<span><img /><input type='date' /></span>
+				</div>
+				<div class='col-3'>
+					<span><img /><input type='date' /></span>
+				</div>
+				<div class='col-3'>
+					<button type='submit'>Забронировать</button>
+				</div>
+			</div>
+		</form>
+	</div>
+	<div class='frame frame-3 text-light'>
+		<div class='div-caption'>
+			<div class='row'>
+				<div class='col-6 border border-light'>
+					<span class='text-light'>Номера</span>
+				</div>
+				<div class='col-6 bg-light'>
+					<span><a class='text-dark' href='#'>Все номера</a></span>
+				</div>
+			</div>
 		</div>
+		<div id = 'frame-3-cards'>
+		<div class='container'>
+			<div class='row'>
+				<div class = 'col-md-4'>
+					<Typecard 
+						image="luxe.png"
+						caption="Аппартаменты класса Luxe"
+						view="Вид на море"
+						size="150 m2"
+						suits="до 4 человек"
+						food="Питание полное"
+						link="#"
+					/>
+				</div>
+				<div class = 'col-md-4'>
+					<Typecard
+						image="gold.png"
+						caption="Аппартаменты класса Gold"
+						view="Вид на море"
+						size="200 m2"
+						suits="До 6 человек"
+						food="Питание полное"
+						link="#"
+					/>
+				</div>
+				<div class = 'col-md-4'>
+					<Typecard
+						image="gold.png"
+						caption="Аппартаменты класса Platina"
+						view="Вид на море"
+						size="250 m2"
+						suits="До 8 человек"
+						food="Питание полное"
+						link="#"
+					/>
+				</div>
+			</div>
+		</div>
+		</div>
+		<img src='../static/img/index3.png'>
 	</div>
-	<div class = 'frame frame-3 text-light'>
-		
-		<img src = '../static/img/index3.png'>
+	<div class='frame frame-4 text-light'>
+
+		<img src='../static/img/index4.png'>
 	</div>
-	<div class = 'frame frame-4 text-light'>
-	
-		<img src = '../static/img/index4.png'>
-	</div>
-	<div class = 'frame frame-5 text-light'>
-	
-		<img src = '../static/img/index5.png'>
+	<div class='frame frame-5 text-light'>
+
+		<img src='../static/img/index5.png'>
 	</div>
 </template>
 
 <script>
 	import Navbar from '../components/Navbar.vue'
+	import Typecard from '../components/Typecard.vue'
 
 	export default {
 		components: {
-			Navbar
+			Navbar,
+			Typecard
 		}
 	}
 </script>
@@ -51,7 +118,7 @@
 		filter: brightness(40%);
 	}
 
-	.frame-1 img{
+	.frame-1 img {
 		position: absolute;
 		top: 0;
 		left: 0;
@@ -78,6 +145,15 @@
 		z-index: -1;
 	}
 
+	#frame-3-cards {
+		position: relative;
+		top: 12%;
+		left: 0;
+		width: 100%;
+		height: 70%;
+		overflow: auto;
+	}
+
 	.frame-4 img {
 		position: absolute;
 		top: 300%;
@@ -97,9 +173,11 @@
 	}
 
 	.index-form {
+		position: absolute;
 		background: white;
-		margin-top: -0.5%;
-		height: 5%;
+		top: 196%;
+		left: 20%;
+		height: 8%;
 		width: 60%;
 	}
 
@@ -108,6 +186,26 @@
 		display: inline-block;
 		margin-top: 3%;
 		padding: 1% 2% 1% 2%;
+	}
+
+	.div-caption {
+		position: relative;
+		top: 5%;
+		left: 0;
+		padding: 1% 2% 1% 2%;
+		width: 40%;
+		text-align: center;
+		vertical-align: middle;
+		line-height: 400%;
+	}
+
+	.div-caption span {
+		font-size: 4vw;
+	}
+
+	.div-caption span a {
+		text-decoration: none;
+		font-size: 2vw;
 	}
 
 	.main-caption {
@@ -132,5 +230,30 @@
 		top: 0;
 		left: 54%;
 		width: 45%;
+	}
+
+	@media (max-width: 991.98px) {
+		.index-form {
+			display: none;
+		}
+
+		.description-1,
+		.description-2 {
+			font-size: 2vh;
+		}
+	}
+
+	@media (max-width: 575.98px) {
+
+		.description-1,
+		.description-2 {
+			font-size: 100%;
+		}
+	}
+
+	@media (max-width: 767.98px) {
+		.div-caption span a {
+			font-size: 75%;
+		}
 	}
 </style>
